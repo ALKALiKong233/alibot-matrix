30778bb138fba20d10ec3c511776c9e63c5b341a
ALKALiKong <alikongalk@gmail.com>
2022-04-15 11:25:27 +0800

    buildbot: Send sync log directly to matrix

--- buildbot.py
+++ buildbot.py
@@ -32,15 +32,10 @@ async def repo(room, message):
     if match.is_not_from_this_bot() and match.prefix() and match.command("sync"):
         os.chdir(buildinfo['dir'])
         if not match.args():
-            sync = os.system(str(buildinfo['proxy']) + ' repo sync --force-sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch | tee sync.log ')
+            sync = os.popen(str(buildinfo['proxy']) + ' repo sync --force-sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch | tee sync.log ').read()
         else:
-            sync = os.system(str(buildinfo['proxy']) + ' repo sync --force-sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch ' + " ".join(match.args()) + ' | tee sync.log ')
-        if sync ==0:
-            await alibuildbot.api.send_text_message(room.room_id, 'Sync Status: Succeed')
-        else:
-            await alibuildbot.api.send_text_message(room.room_id, 'Sync Status: Failed')
-        url = PB.create_paste_from_file('sync.log', 0, None, None, None)
-        await alibuildbot.api.send_text_message(room.room_id, 'Sync log:' + url)
+            sync = os.popen(str(buildinfo['proxy']) + ' repo sync --force-sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch ' + " ".join(match.args()) + ' | tee sync.log ').read()
+        await alibuildbot.api.send_text_message(room.room_id, sync)
 
 @alibuildbot.listener.on_message_event
 async def build(room, message):
